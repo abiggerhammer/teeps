082b0bb0624f3b50e31728eba720824d3d66b53e
Meredith L. Patterson <meredith@mautinoa.com>
2019-02-28 04:20:07 +0100

    trying 0mq

--- teepserver.py
+++ teepserver.py
@@ -25,7 +25,7 @@ def listen(label=None):
 
 @app.route('/say', methods=['POST'])
 def say(speaker=None, speech=None):
-    pass
+    teep.send_string(speech)
 
 # Socket to receive presence notifications/speech actions over
 remote_pull = ctx.socket(zmq.PULL)
